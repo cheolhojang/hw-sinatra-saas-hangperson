0b65f794669989e762cb81e78a719f30d8bf6bf6
CJ Jang <cjjang@CJs-MacBook-Pro.home>
2019-02-10 00:30:39 -0800

    first commit

--- app.rb
+++ app.rb
@@ -40,7 +40,22 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    redirect '/show'
+    if letter.nil? || !letter.index(/[^a-zA-Z]+/).nil?
+      flash[:message] = "Invalid guess."
+      redirect '/show'
+    elsif !@game.guess(letter) 
+      flash[:message] = "You have already used that letter."
+      redirect '/show'
+    else
+      if @game.check_win_or_lose == :win
+        redirect '/win'
+      elsif @game.check_win_or_lose == :lose
+        redirect '/lose'
+      else 
+        redirect '/show'
+      end
+    end
+
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
